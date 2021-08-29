04f57e03db07698f98e9595348c0d47db7678592
davekch <dave-koch@web.de>
2021-08-29 13:43:39 +0200

    init

---
+++ README.md
@@ -0,0 +1,35 @@
+# Music Guessing Game
+
+Music guessing game inspired by [David Bennett Piano](https://www.youtube.com/watch?v=O5h7nQdWhKg&t=587s).  
+
+Listen to a short random section of a randomly selected song and guess which song it is!
+
+### Usage
+
+Place a bunch of audio files in a folder of your choice (should not contain anything else) and run `python musicguessinggame.py myfolder`.
+
+```
+$ python musicguessinggame.py --help
+usage: Music guessing game [-h] [--duration DURATION]
+                           [--attempts ATTEMPTS]
+                           folder
+
+positional arguments:
+  folder               path to folder where sound files are stored
+
+optional arguments:
+  -h, --help           show this help message and exit
+  --duration DURATION  duration of sound samples in seconds
+  --attempts ATTEMPTS  number of attempts
+
+```
+
+
+### Installation
+
+requires Python3.8 or newer
+
+```
+git clone https://github.com/davekch/musicguessinggame.git
+pip install pydub
+```
---
+++ musicguessinggame.py
@@ -0,0 +1,45 @@
+from pydub import AudioSegment
+from pydub.playback import play
+import random
+import os
+from argparse import ArgumentParser
+
+
+def get_random_snippet_from_file(path, duration):
+    audio = AudioSegment.from_file(path)
+    end_time_ms = int(audio.duration_seconds) * 1000
+    start_time_ms = random.randint(0, end_time_ms - duration * 1000)
+    return audio[start_time_ms:(start_time_ms+duration*1000)]
+
+
+def guessinggame(folder, duration, attempts):
+    songs = os.listdir(folder)
+    while songs:
+        song = random.choice(songs)
+        songs.remove(song)
+        snippet = get_random_snippet_from_file(os.path.join(folder, song), duration)
+        for i in range(attempts):
+            input(f"press <Enter> to listen to snippet ({attempts-i} attempt(s) left)")
+            play(snippet)
+        input("press <Enter> to see solution")
+        print(f"The song was {song}!")
+        print()
+    print("That's it! You made a guess on all the songs")
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser("Music guessing game")
+    parser.add_argument("folder", help="path to folder where sound files are stored")
+    parser.add_argument("--duration", type=float, default=2, help="duration of sound samples in seconds")
+    parser.add_argument("--attempts", type=int, default=2, help="number of attempts")
+    args = parser.parse_args()
+
+    print("Welcome to the great music guessing game!")
+    print()
+    print("press <Ctrl+C> to quit")
+    try:
+        guessinggame(args.folder, args.duration, args.attempts)
+    except KeyboardInterrupt:
+        print()
+        print()
+        print("goodbye!")
